a4683fe70362ad9010c3735db4807105ecb00e25
邓承克 <dengchengke@dengchengkedeMacBook-Pro.local>
2026-04-07 17:28:38 +0800

    fix: remove unused library

--- quartz/util/path.ts
+++ quartz/util/path.ts
@@ -1,7 +1,6 @@
 import { slug as slugAnchor } from "github-slugger"
 import type { Element as HastElement } from "hast"
 import { clone } from "./clone"
-import { match } from "node:assert"
 
 // this file must be isomorphic so it can't use node libs (e.g. path)
 
